468102585c3aef8c6fe5b636f0571a530eff06c4
Pradeep Yadav <69448799+webpradeep@users.noreply.github.com>
2023-11-12 17:09:34 +0000

    Datatype Done

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict"
+// alert(3+3)  we are using node , not browser
+
+let name ="pradeep"; //String
+let age = 233; //Number
+let isActive = true; //Boolean
+
+let valum= null // standalone value 
+let a ;    // Undefined value
+ 
+
+// primitive
+
+
+// String
+// Number 
+// BigInt 
+// Boolean 
+// Null 
+// undefined 
+// Symbol
+
+
+// Non primitive
+
+// Object
