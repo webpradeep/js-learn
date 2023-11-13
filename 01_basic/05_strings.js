708e9fa722db230e95840549cf771424d6f336e3
Pradeep Yadav <69448799+webpradeep@users.noreply.github.com>
2023-11-13 16:23:08 +0000

    Strings Done

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,12 @@
+const name = "Pradeep"
+const repoCount= 50
+
+console.log(name + repoCount)
+
+console.log(`my name is ${name} and course count is ${repoCount}`)
+console.log(name[0]);
+console.log(name.charAt(2));
+console.log(name.indexOf("d"));
+console.log(name.slice(-8,4))
+console.log(name.substring(0,4))
+// console.log(name.charAt(2))
---
+++ 01_basic/dataType-summary.js
@@ -0,0 +1,21 @@
+// # Primative Data type 
+
+
+// 7 type:  (pass by Value)
+
+// String 
+// Numbar
+// Boolean 
+// Null 
+// Undefined
+// BigInt
+// Symbol
+
+// Non Primative  (pass by reference)
+
+// Array 
+// Object 
+// Functions 
+
+
+const heros = ["saktiman","Nagraj", "doga"];
