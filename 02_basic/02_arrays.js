e4dc460630cc1f21e6f812f7acb6775ef2c199e0
Pradeep Yadav <69448799+webpradeep@users.noreply.github.com>
2023-11-13 17:42:42 +0000

    Array 2 done

---
+++ 02_basic/02_arrays.js
@@ -0,0 +1,15 @@
+const marvels= ["Ironman", "thor", "Spiderman"];
+const dcs= ["superman", "flash", "batman"];
+
+// marvels.push(dcs)
+// console.log(marvels)
+
+// Push add second array as array /
+
+const newarr = marvels.concat(dcs)
+console.log(newarr);
+
+
+// from 
+// of
+// isArray
