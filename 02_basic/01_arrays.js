b989db2180e7e4849c62b8c55b2694bd25f318b4
Pradeep Yadav <69448799+webpradeep@users.noreply.github.com>
2023-11-13 17:26:04 +0000

    Array Basic 1 Done

---
+++ 02_basic/01_arrays.js
@@ -0,0 +1,29 @@
+// Arrays
+
+const myArr = [0,1,2,3,4,5]
+
+console.log(myArr[0])
+
+// Arrays Method 
+
+myArr.push(5)
+myArr.pop()
+myArr.unshift(8) // add begning
+myArr.shift() // remove
+myArr.includes(9)
+myArr.indexOf(3)
+// console.log(myArr)
+
+
+// slice and splice 
+
+const arr = [1,2,3,4,5]
+
+const arr1 = arr.slice(1,3) 
+console.log("A", arr)
+console.log(arr1)
+
+const arr2 = arr.splice(1,3)
+console.log("B", arr)
+console.log(arr2)
+ 
